f7af327c4ce64174f6a6f441ae2fec65326024b5
Jorwin Reyes <jorwin2002@gmail.com>
2024-09-07 16:31:17 -0400

    Project Setup

---
+++ or-llc/src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './navbar.css'
+const navbar = () => {
+  return (
+    <nav>
+      <img></img>
+    </nav>
+  )
+}
+import './navbar.css'
+export default navbar
