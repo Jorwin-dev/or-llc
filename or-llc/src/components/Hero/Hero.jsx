d9721850a58f3eb4fc14575f080b9fdc44d06125
Jorwin Reyes <jorwin2002@gmail.com>
2024-09-08 18:45:35 -0400

    Beinning of Hero section

---
+++ or-llc/src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      
+    </div>
+  )
+}
+
+export default Hero
