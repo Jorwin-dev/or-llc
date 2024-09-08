fc33f9fc3f4cd2c1c611259581c27eb72e768bf4
Jorwin Reyes <jorwin2002@gmail.com>
2024-09-08 18:27:21 -0400

    Navbar Creation

--- or-llc/src/components/navbar/navbar.jsx
+++ or-llc/src/components/navbar/navbar.jsx
@@ -5,15 +5,15 @@ import logo from '/Users/Jorwi/or-llc/or-llc/src/assets/O&R_Logo_no_number.png'
 
 const navbar = () => {
   return (
-    <nav>
-      <img src={logo} alt='logo'/>
+    <nav className='container'>
+      <img src={logo} alt='logo' className='logo'/>
       <ul>
         <li>Home</li>
         <li>About Us</li>
         <li>Services</li>
         <li>Testimonials</li>
         <li>Showcase</li>
-        <li>Contact Us</li>
+        <li><button className='btn'>Contact Us</button></li>
       </ul>
     </nav>
   )
