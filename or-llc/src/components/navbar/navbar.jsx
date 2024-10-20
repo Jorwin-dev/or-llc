5dd1b6685515ea9ff029b13b53507ff4615d304c
Jorwin Reyes <jorwin2002@gmail.com>
2024-10-20 19:30:15 -0400

    Completed Base

--- or-llc/src/components/navbar/navbar.jsx
+++ or-llc/src/components/navbar/navbar.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import './navbar.css'
 import logo from '../../assets/O&R_Logo_no_number.png'
+import { Link } from 'react-scroll'
 
 const navbar = () => {
 
@@ -18,12 +19,12 @@ const navbar = () => {
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
       <img src={logo} alt='logo' className='logo'/>
       <ul>
-        <li>Home</li>
-        <li>Services</li>
-        <li>About us</li>
-        <li>Showcase</li>
-        <li>Testimonials</li>
-        <li><button className='btn'>Contact Us</button></li>
+        <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+        <li><Link to='services' smooth={true} offset={-260} duration={500}>Services</Link></li>
+        <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
+        <li><Link to='showcase' smooth={true} offset={-260} duration={500}>Showcase</Link></li>
+        <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+        <li><Link to='contact' smooth={true} offset={-260} duration={500}>Contact Us</Link></li>
       </ul>
     </nav>
   )
